3db4bfc78fd6e8ee1138b792c84a3a88002c3f3a
djavid <dpkhalilov@edu.hse.ru>
2017-11-05 04:28:52 +0300

    git .ignore fix

--- app/src/main/java/com/djavid/bitcoinrate/view/interfaces/TickerFragmentView.java
+++ app/src/main/java/com/djavid/bitcoinrate/view/interfaces/TickerFragmentView.java
@@ -2,6 +2,7 @@ package com.djavid.bitcoinrate.view.interfaces;
 
 import android.content.res.Resources;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 
 import com.djavid.bitcoinrate.core.ScrollView;
 import com.djavid.bitcoinrate.core.View;
@@ -11,4 +12,5 @@ import com.mindorks.placeholderview.PlaceHolderView;
 
 public interface TickerFragmentView extends ScrollView<TickerItemRealm> {
     void showSnackbar(String message);
+    SwipeRefreshLayout getRefreshLayout();
 }
